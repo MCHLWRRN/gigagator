6b91db3441ce7bda0e6af4c47c880ef6a79bdb53
Your Name <me@mchlwrrn.com>
2014-11-29 05:43:26 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gigagator::Application.config.secret_key_base = '731a17dde40d525b7ec9aef373fc3cb6b09e3c45a1ba64e6021b67d9c6be38624ede0115269bc872aefd9048c32c73497fad221343b39413ea7d4e6828eaae4c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gigagator::Application.config.session_store :cookie_store, key: '_gigagator_session'
