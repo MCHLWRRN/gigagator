b6be3afdabcd78083f4afa384d4595d42c27b95f
MCHLWRRN <me@mchlwrrn.com>
2015-03-08 18:36:20 -0700

    Hooking up venue show page

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -2,4 +2,14 @@ class VenuesController < ApplicationController
 	def new
 		@venue = Venue.new
 	end
+
+	def show
+		@venue = Venue.find(params[:id])
+	end
+
+	private
+
+	def venue_params
+		params.require(:place).permit(:name, :description, :address)
+	end
 end
